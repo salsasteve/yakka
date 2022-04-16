967261cbe4edd65e97b423ef647b5f3553a96b4d
Salsa Steve <salsasteve@metacanny.com>
2022-04-16 13:32:01 -0500

    removing firebase

--- src/firebase/dto/update-firebase.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateFirebaseDto } from './create-firebase.dto';
-
-export class UpdateFirebaseDto extends PartialType(CreateFirebaseDto) {}
