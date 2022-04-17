51dba9f548892e3db410c2d65f84cab941d748b6
Salsa Steve <salsasteve@metacanny.com>
2022-04-16 22:12:40 -0500

    Adding valid websites to cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,18 @@ import { YakkaBot } from './yakkabot.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(YakkaBot);
+  app.enableCors({
+    origin: [
+      'http://localhost:4000',
+      'http://orca.watch',
+      'http://www.orca.watch',
+      'https://localhost:4000',
+      'https://orca.watch',
+      'https://www.orca.watch',
+    ],
+    methods: ['GET', 'POST'],
+    credentials: true,
+  });
   await app.listen(3000);
 }
 bootstrap();
