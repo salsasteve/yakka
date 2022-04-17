a38ff3d1d1d35d212ef7ba8ae863a5147919dc3a
Salsa Steve <salsasteve@metacanny.com>
2022-04-17 13:59:14 -0500

    Added function for whitelisted CORS

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,17 @@ import { YakkaBot } from './yakkabot.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(YakkaBot);
+  const whitelist = ['http://localhost:4000', '\\.orca\\.watch$'];
   app.enableCors({
-    origin: ['\\.localhost\\:4000$', '\\.orca\\.watch$'],
+    origin: function (origin, callback) {
+      if (!origin || whitelist.indexOf(origin) !== -1) {
+        console.log('allowed cors for:', origin);
+        callback(null, true);
+      } else {
+        console.log('blocked cors for:', origin);
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type', 'Accept', 'Origin'],
     credentials: true,
