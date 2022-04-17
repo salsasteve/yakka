9e19a9829a7ebffb0f3c8f0255341c6a7ccb9ec2
Salsa Steve <salsasteve@metacanny.com>
2022-04-17 12:29:51 -0500

    shorten cors with regex

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,7 @@ import { YakkaBot } from './yakkabot.module';
 async function bootstrap() {
   const app = await NestFactory.create(YakkaBot);
   app.enableCors({
-    origin: [
-      'http://localhost:4000',
-      'http://orca.watch',
-      'http://www.orca.watch',
-      'https://localhost:4000',
-      'https://orca.watch',
-      'https://www.orca.watch',
-    ],
+    origin: ['\\.localhost\\:4000$', '\\.orca\\.watch$'],
     methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type', 'Accept', 'Origin'],
     credentials: true,
